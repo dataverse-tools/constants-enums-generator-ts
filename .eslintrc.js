f2fdd23f670288340c04473b43244483305d5800
Anton Zhaparov <zhaparoff@gmail.com>
2021-08-24 10:23:37 +0300

    Linter config updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "tsconfig.json",
-    ecmaVersion: 2020,
+    ecmaVersion: "latest",
     sourceType: "module"
   },
   rules: {
